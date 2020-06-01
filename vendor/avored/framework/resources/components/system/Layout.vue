c424d3a1e3027b1b845973022a8de3904b72d624
Rishikesh <rishiesh.pal@unicoconnect.com>
2020-06-01 06:31:25 +0530

    cj

---
+++ vendor/avored/framework/resources/components/system/Layout.vue
@@ -0,0 +1,29 @@
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+    data() {
+        return {
+            collapsed: false,
+        }
+    },
+
+    mounted() {
+        // let data = this.$apollo.query({
+        //     query: gql `query AllCategories {
+        //         allCategory { 
+        //             id 
+        //             name
+        //             slug
+        //             meta_title
+        //             meta_description
+        //         }
+        //     }`
+        // }).then(res => {
+        //     console.log(res.data.allCategory);
+
+        // });
+    }
+}
+</script>
